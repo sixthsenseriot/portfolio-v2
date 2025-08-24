18a11b393737a6f23dcf175668e86884e85fc31d
Khanh <khanh@starchdev.com>
2025-08-24 15:36:26 -0500

    add: Build out resume page to include resume

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,5 +1,30 @@
+import "./ResumePage.css";
+
 const ResumePage = () => {
-    return <div className="ResumePage"></div>;
+    return (
+        <div className="ResumePage">
+            <section className="ResumePage">
+                <div className="resume-container text-center m-5">
+                    <h1 className="projects-header text-center mb-4">
+                        My Resume
+                    </h1>
+
+                    <a
+                        href="https://drive.google.com/file/d/1Mw48nQyFytvXTf44TT7EBKMdFaUORNch/view?usp=sharing"
+                        target="_blank"
+                        rel="noreferrer"
+                        title="Khanh Nguyen's Resume"
+                    >
+                        <img
+                            src="/images/resume2025.png"
+                            alt="Khanh Nguyen's Resume"
+                            className="resume-img enhance-img project-img img-fluid"
+                        />
+                    </a>
+                </div>
+            </section>
+        </div>
+    );
 };
 
 export default ResumePage;
